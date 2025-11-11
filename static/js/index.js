6d51a6560e82efc8cb73db0101b0f2e0f5c244c2
shihao <shihao@megvii.com>
2025-11-11 13:34:18 +0800

    init

--- static/js/index.js
+++ static/js/index.js
@@ -75,4 +75,18 @@ $(document).ready(function() {
 
     bulmaSlider.attach();
 
+    // ======= Collapsible Toggle =======
+    var coll = document.getElementsByClassName("collapsible");
+    for (let i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("is-active");
+        var content = this.parentElement.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+
 })
